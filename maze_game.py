96401bf9ca8f9644a781f902bdc42703f23d6553
Juan Ricrado Palumbo <juanrpalumbo@gmail.com>
2019-09-25 00:54:24 +0200

    Initial commit in class

--- maze_game.py
+++ maze_game.py
@@ -1,7 +1,7 @@
 import random
 directions = ("N","S","E","W")
-answer="you are in a maze, choose one direction (N, S, E, or W)"+random.choice(directions)
-combination('N','S','N','E','W','N')
+answer=("you are in a maze, choose one direction (N, S, E, or W)"+random.choice(directions))
+combination=('N','S','N','E','W','N')
 moves=1
 lives=3
 
@@ -10,7 +10,7 @@ if moves ==(11,21,31):
     print('you have just los one life, you now have'+str(lives)+'lives')
 
     for i in combination:
-        while answer == combination(combination.index[i])
+        while answer == combination[combination.index(i)]
             print ("start again")
             x=x+1
             if lives == 0:
